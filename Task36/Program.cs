1a226344af775835c8740a1ff91e07be98f05363
Yar_Sanych_NB <y.krivoff@gmail.com>
2022-09-05 13:44:20 +0300

    Edited Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -27,7 +27,7 @@ void PrintArray(int[] arr)
 int SumOddElemet(int[] arr)
 {
     int sum = 0;
-    for(int i = 0; i < arr.Length; i = i + 2)
+    for(int i = 1; i < arr.Length; i += 2)
         sum += arr[i];
     return sum;
 }
